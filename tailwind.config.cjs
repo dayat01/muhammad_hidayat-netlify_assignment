9783debe27ccfa5d98e2d1cd221685718e6257c1
dayat01 <113222954+dayat01@users.noreply.github.com>
2022-09-30 10:29:36 +0700

    commit assignment

---
+++ tailwind.config.cjs
@@ -0,0 +1,15 @@
+//  File to enable tailwindcss intellisense
+module.exports = {
+  theme: {
+    screens: {
+      'hanphone': '320px',
+      // => @media (min-width: 640px) { ... }
+
+      'laptop': '1080px',
+      // => @media (min-width: 1024px) { ... }
+
+      'desktop': '1280px',
+      // => @media (min-width: 1280px) { ... }
+    },
+  }
+}
